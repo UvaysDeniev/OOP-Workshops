52922771180cf6fe08807733bb615874220d30e5
Uvays Deniev <udeniev@myseneca.ca>
2023-10-17 19:45:14 -0400

    v3
    Complete version

--- WS05/Workshop 5 Part 2/Portfolio.cpp
+++ WS05/Workshop 5 Part 2/Portfolio.cpp
@@ -22,6 +22,7 @@ Date        : 2023-10-17
 
 #include <ostream> // for std ostream
 #include <iostream> // for cout
+#include <cstring> // for strcpy
 #include "Portfolio.h"
 
 
@@ -79,8 +80,107 @@ namespace sdds {
       return cout;
    }
 
+   // The result would be the value of the Portfolio m_value.
+   // This operator can not modify the Portfolio object.
+   Portfolio::operator double() const
+   {
+      return m_value;
+   }
+
+   // The result would be the name of the stock m_stock.
+   // This operator can not modify the Portfolio object.
+   Portfolio::operator const char* () const
+   {
+      return m_stock;
+   }
+
+   // The result would be the type of investment m_type.
+   // This operator can not modify the Portfolio object.
+   Portfolio::operator char() const
+   {
+      return m_type;
+   }
+
+   // This will return TRUE if the type of the investment m_type is Growth 'G' or Value 'V or Investment 'I'.
+   // For any other types, it will return FALSE.
+   Portfolio::operator bool() const
+   {
+      return m_type == 'G' || m_type == 'V' ||  m_type == 'I';
+   }
 
 
+   // The bool operator ~() will return true if m_value is negative.
+   // This operator can not modify the Portfolio object.
+   bool Portfolio::operator~() const
+   {
+      return m_value < 0;
+   }
 
+   // if the Portfolio does not evaluate TRUE or the double value is negative, the action is omitted.
+   // Reference of the Portfolio is returned after the operation.
+   Portfolio& Portfolio::operator+=(double pValue)
+   {
+      // Checks if the current Portfolio object is valid and the input value is non-negative
+      if (*this && pValue >= 0) {
+         m_value += pValue;
+      }
+      return *this;
+   }
+
+   // if the Portfolio does not evaluate TRUE or the double value is negative, the action is omitted.
+   // Reference of the Portfolio is returned after the operation.
+   Portfolio& Portfolio::operator-=(double nValue)
+   {
+      // Checks if the current Portfolio object is valid and the input value is non-negative
+      if (*this && nValue >= 0) {
+         m_value -= nValue;
+      }
+      return *this;
+   }
+
+   // After this operation, the total investment value of the Portfolio on the left will be the sum of both Portfolios.
+   // The Portfolio on the right is set to empty.
+   // It is not allowed to invest values from a Portfolio back to itself. 
+   // If any of the two portfolios don't evaluate to TRUE, no action should be taken.
+   Portfolio& Portfolio::operator<<(Portfolio& rhs)
+   {
+      // Ensure the portfolios are different, and both portfolios evaluate to TRUE
+      if (this != &rhs && *this && rhs) {
+         m_value += rhs.m_value; // Transfer the value from rhs portfolio to this portfolio
+         rhs.emptyPortfolio(); //rhs reset
+      }
+      return *this;
+   }
+
+   // After this operation, the total investment value of the Portfolio in the right will be the sum of both Portfolios.
+   // The Portfolio on the left is set to empty.
+   // It is not allowed to invest values from a Portfolio back to itself.If any of the two portfolios don't evaluate to TRUE, no action should be taken.
+   Portfolio& Portfolio::operator>>(Portfolio& rhs)
+   {
+      // Ensure the portfolios are different, and both portfolios evaluate to TRUE
+      if (this != &rhs && *this && rhs) {
+         rhs.m_value += m_value; // Transfer the value from rhs portfolio to this portfolio
+         emptyPortfolio();
+      }
+      return *this;
+   }
+
+   // The double value should be the sum of the m_values of the two Flights.
+   // If any of the two Portfolios don't evaluate to TRUE, then zero is returned.
+   double operator+(const Portfolio& lhs, const Portfolio& rhs) {
+      if (lhs && rhs) {
+         return double(lhs) + double(rhs);
+      }
+      return 0;
+   }
+
+   // The $dollar investment value of the right operand(Portfolio reference) should be added to the left operand(double reference)
+   // Then the value of the double reference is returned.
+   double operator+=(double& value, const Portfolio& portfolio) {
+      if (portfolio) {
+         value += double(portfolio);
+      }
+      return value;
+   }
 
 }
--- WS05/Workshop 5 Part 2/Portfolio.h
+++ WS05/Workshop 5 Part 2/Portfolio.h
@@ -29,70 +29,53 @@ namespace sdds {
       char m_stock[16];
       char m_type;
 
-      void dispPortfolio() const;
+      void emptyPortfolio();
 
    public:
 
       Portfolio();
-      void emptyPortfolio();
       Portfolio(double value, const char* stock, char type);
 
+      void dispPortfolio() const;
       std::ostream& display() const;
 
-    /****Operatorand conversion overloads****/
+      /****Operatorand conversion overloads****/
 
-       //  Portfolio can be casted to a double(double type).
-       //The result would be the value of the Portfolio m_value.
-       //This operator can not modify the Portfolio object.
+      // Portfolio can be casted to a double(double type).
+      operator double() const;
 
-       //  Portfolio can be casted to C - string(const char* type).
-       //The result would be the name of the stock m_stock.
-       //This operator can not modify the Portfolio object.
+      // Portfolio can be casted to C - string(const char* type).
+      operator const char* () const;
 
-       //  Portfolio can be casted to a char(char type).
-       //The result would be the type of investment m_type.
-       //This operator can not modify the Portfolio object.
+      // Portfolio can be casted to a char(char type).
+      operator char() const;
 
-       //  Portfolio can be casted to a bool(boolean type).
-       //This will return TRUE if the type of the investment m_type is Growth 'G' or Value 'V or Investment 'I'.
-       //For any other types, it will return FALSE.
+      // Portfolio can be casted to a bool(boolean type).
+      operator bool() const;
 
-       //  A double can be added to the value of the Portfolio m_value using += operator;
-       //if the Portfolio does not evaluate TRUE or the double value is negative,
-       //the action is omitted.
-       //Reference of the Portfolio is returned after the operation.
+      // The bool operator ~() will return true if m_value is negative.
+      // This operator can not modify the Portfolio object.
+      bool operator~() const;
 
-       //  A double can be subtracted from the value of the Portfolio m_value using -= operator;
-       // if the Portfolio does not evaluate TRUE or the double value is negative, 
-       //the action is omitted.
-       //Reference of the Portfolio is returned after the operation.
+      // A double can be added to the value of the Portfolio m_value using += operator;
+      Portfolio& operator+=(double pValue);
 
-       //  The bool operator ~() will return true if m_value is negative.
-       // This operator can not modify the Portfolio object.
+      // A double can be subtracted from the value of the Portfolio m_value using -= operator;
+      Portfolio& operator-=(double nValue);
 
-      //  overload the << operator* (left shift operator) to move $dollar investment value m_value from Portfolio in the right to the left.
-      //After this operation, the total investment value of the Portfolio on the left will be the sum of both Portfolios.
-      //The Portfolio on the right is set to empty.
-      // It is not allowed to invest values from a Portfolio back to itself. 
-      //If any of the two portfolios don't evaluate to TRUE, no action should be taken.
+      // overload the << operator* (left shift operator) to move $dollar investment value m_value from Portfolio in the right to the left.
+      Portfolio& operator<<(Portfolio& rhs);
 
-
-
-      //overload the >> operator* (right shift operator) to move $dollar investment value m_value from Portfolio in the left to the right.
-      //After this operation, the total investment value of the Portfolio in the right will be the sum of both Portfolios.
-      //The Portfolio on the left is set to empty.
-      //It is not allowed to invest values from a Portfolio back to itself.If any of the two portfolios don't evaluate to TRUE, no action should be taken.
+      // overload the >> operator* (right shift operator) to move $dollar investment value m_value from Portfolio in the left to the right.
+      Portfolio& operator>>(Portfolio& rhs);
+   };
 
    /****Binary helper operators****/
 
-       // create a binary stand alone helper + operator that accepts a constant Portfolio reference at left and another constant Portfolio reference at right and returns a double value.
-       //  The double value should be the sum of the m_values of the two Flights.
-       //  If any of the two Portfolios don't evaluate to TRUE, then zero is returned.
+   // create a binary stand alone helper + operator that accepts a constant Portfolio reference at left and another constant Portfolio reference at right and returns a double value.
+   double operator+(const Portfolio& left, const Portfolio& right);
 
-       //  create a binary stand alone helper += operator that accepts a double reference at left and a constant Portfolio reference at right and returns a double value.
-       //  The $dollar investment value of the right operand(Portfolio reference) should be added to the left operand(double reference)
-       //  Then the value of the double reference is returned.
-
-   };
+   // create a binary stand alone helper += operator that accepts a double reference at left and a constant Portfolio reference at right and returns a double value.
+   double operator+=(double& value, const Portfolio& portfolio);
 }
 #endif // SDDS_Portfolio_H_
