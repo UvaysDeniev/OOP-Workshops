9c3669e817ebe9ccafc180f712f08387329c3cbc
Uvays Deniev <udeniev@myseneca.ca>
2023-10-17 16:41:31 -0400

    v1

---
+++ WS05/Workshop 5 Part 2/Portfolio.h
@@ -0,0 +1,36 @@
+/***********************************************************************
+// OOP244 Workshop #5 DIY (part 2): tester program
+//
+// File  Portfolio.h
+// Version 1.0
+// Author   Asam Gulaid, revised by Fardad Soleimanloo
+// Description
+//
+// Revision History
+// -----------------------------------------------------------
+// Name                 Date            Reason
+***********************************************************************/
+/*****************************************************************************
+                           WORKSHOP 5 - PART 2
+Author	    : Uvays Deniev
+Student ID# : 123026221
+Email       : udeniev@myseneca.ca
+Section     : NCC
+Date        : 2023-10-17
+*****************************************************************************/
+#ifndef SDDS_Portfolio_H_
+#define SDDS_Portfolio_H_
+
+
+namespace sdds {
+
+   class Portfolio {
+      double m_value = 0;
+      char m_stock[16];
+      char m_type;
+
+   public:
+
+   };
+}
+#endif // SDDS_Portfolio_H_
